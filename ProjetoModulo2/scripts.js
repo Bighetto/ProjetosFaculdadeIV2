7fa7cda8d565edd52e049d13f150712cfcabde4f
Arthur Bighetto <arthurbighetto36@gmail.com>
2021-06-04 17:21:01 -0300

    Adicionado Projetos 2 e 3 da Facul IV2

---
+++ ProjetoModulo2/scripts.js
@@ -0,0 +1,11 @@
+var h1 = document.getElementById ("h1")
+
+var btn = document.getElementById ("btn1")
+
+function clicou(){
+   if (h1.innerHTML===""){
+    h1.innerHTML = "oi"
+   } else {      
+       h1.innerHTML = ""
+   }
+}
